ce0d8be380f06ddba086b706429cd062c54a2d8d
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-10 13:04:39 +0200

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication1.Controllers;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
